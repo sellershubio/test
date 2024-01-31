767e3bb3343dfc1fbbe38985da31474b82b6712e
Vamshi <thogaru.vamshi@gmail.com>
2024-01-31 17:23:14 +0530

    test

--- AASAAN/src/main/java/Base/BaseTest.java
+++ AASAAN/src/main/java/Base/BaseTest.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-
 import Utils.Constants;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
